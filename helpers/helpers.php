2a2df596f49f922ff2436ae3bf4403cc941a6923
Sebastian Hilger <hilger@kraenk.de>
2021-02-05 16:49:42 +0100

    Update helpers.php

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -74,6 +74,11 @@ function nova_cms_magify_links($str, $open_urls_in_new_tab = false)
                 }
                 $str = str_replace($url, $newUrl, $str);
             }
+
+            if ($open_urls_in_new_tab && substr($url, 0, 4) == 'http') {
+                $newUrl = $url.'" target="_blank';
+                $str = str_replace($url, $newUrl, $str);
+            }
         }
     }
 
