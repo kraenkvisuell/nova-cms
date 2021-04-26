843b9e99640a44700d27afc062415703d1c55d99
Sebastian Hilger <hilger@kraenk.de>
2021-04-26 13:05:52 +0200

    remove obsolete folder

--- resources/themes/default/views/components/layout/layout.blade.php
+++ resources/themes/default/views/components/layout/layout.blade.php
@@ -17,8 +17,7 @@
     <meta content="" name="keywords">
 
     <link rel="icon" href="/img/favicon02.png" type="image/png">
-    <link rel="stylesheet" href="https://use.typekit.net/qir7wxz.css">
-
+    
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script src="{{ mix('js/main.js') }}"></script>
--- resources/themes/default/views/parts/content-block.blade.php
+++
@@ -1,8 +0,0 @@
-@props([
-    'contentBlock'
-])
-
-<x-dynamic-component
-    :component="'content-blocks.'.$contentBlock->block"
-    :field="$contentBlock->field"
-/>
