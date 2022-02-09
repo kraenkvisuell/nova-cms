c72765d4eb3326db4dbd477ac37f2303eb63d1b9
Sebastian Hilger <hilger@kraenk.de>
2022-02-09 17:56:00 +0100

    translation

--- resources/lang/nova-cms/de/pages.php
+++ resources/lang/nova-cms/de/pages.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-    'content' => 'Inhalt',
     'browser_title' => 'Browsertitel',
+    'content' => 'Inhalt',
+    'is_home' => 'Ist Startseite',
     'meta_description' => 'Meta-Description',
-    'page' => 'Seite',
     'page_title' => 'Seitentitel',
+    'page' => 'Seite',
     'pages' => 'Seiten',
-    'slug' => 'Text-ID',
     'slug_explanation' => 'wird in der URL verwendet (außer bei der Startseite)',
+    'slug' => 'Text-ID',
     'title' => 'Titel',
 ];
--- resources/lang/nova-cms/en/pages.php
+++ resources/lang/nova-cms/en/pages.php
@@ -3,6 +3,7 @@
 return [
     'content' => 'content',
     'browser_title' => 'browser title',
+    'is_home' => 'is home page',
     'meta_description' => 'meta description',
     'page' => 'page',
     'page_title' => 'page title',
