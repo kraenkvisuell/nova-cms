5f24021d143c152a4eec3b515d6f633b4e23800d
Sebastian Hilger <hilger@kraenk.de>
2022-03-30 17:46:47 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,15 @@ use Kraenkvisuell\NovaCms\Facades\RoutesHelper;
 
 Route::middleware(['web'])->group(function () {
     Route::post('/update-cookie-consent', 'Kraenkvisuell\NovaCms\Controllers\CookiesController@update')->name('update-cookies-consent');
-});
 
-if (
-    ! Request::is(substr(config('nova.path'), 1).'*')
-    && ! Request::is('nova-api/*')
-    && ! Request::is('api/*')
-    && ! Request::is('draft/*')
-) {
-    Route::get('/{locale}'.RoutesHelper::prefix().'/{page}', 'Kraenkvisuell\NovaCms\Controllers\PagesController@show')->name('nova-page-multi');
-    Route::get(RoutesHelper::prefix().'/', 'Kraenkvisuell\NovaCms\Controllers\PagesController@show')->name('nova-page-single-home');
-    Route::get(RoutesHelper::prefix().'/{page}', 'Kraenkvisuell\NovaCms\Controllers\PagesController@show')->name('nova-page-single');
-}
+    if (
+        ! Request::is(substr(config('nova.path'), 1).'*')
+        && ! Request::is('nova-api/*')
+        && ! Request::is('api/*')
+        && ! Request::is('draft/*')
+    ) {
+        Route::get('/{locale}'.RoutesHelper::prefix().'/{page}', 'Kraenkvisuell\NovaCms\Controllers\PagesController@show')->name('nova-page-multi');
+        Route::get(RoutesHelper::prefix().'/', 'Kraenkvisuell\NovaCms\Controllers\PagesController@show')->name('nova-page-single-home');
+        Route::get(RoutesHelper::prefix().'/{page}', 'Kraenkvisuell\NovaCms\Controllers\PagesController@show')->name('nova-page-single');
+    }
+});
