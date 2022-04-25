abe0579fdef96d0a17c4018afb7f5f27a83b1bcb
Sebastian Hilger <hilger@kraenk.de>
2022-04-25 11:47:44 +0200

    cookie trans

--- resources/lang/nova-cms/en/settings.php
+++ resources/lang/nova-cms/en/settings.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
-    'settings' => 'Einstellungen',
-    'website_title' => 'Website-Titel',
+    'accept_and_save_cookies' => 'accept and save settings',
+    'change_cookie_settings' => 'change cookie settings',
+    'close_cookie_settings' => 'close cookie settings',
+    'cookies_explanation' => 'We use cookies to keep out website functional and cookies to optimize the website performance.',
+    'essential' => 'essential',
+    'essential_explanation' => 'These cookies are needed for the basic functions. They cannot be deactivated.',
+    'functional' => 'functional',
+    'functional_explanation' => 'These cookies let us analyse the usage of the website. We never give away user data to third parties. Deactivating these cookies can lead to some content - like videos - not working.',
+    'marketing' => 'marketing',
+    'marketing_explanation' => 'We are categorically never allowing any other party to place markting cookies on our website.',
+    'reset_cookie_settings' => 'reset cookie settings',
+    'settings' => 'settings',
+    'website_title' => 'website title',
 ];
