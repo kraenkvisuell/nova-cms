4df086fbdf98a14f6c772c05982da804bd0bd10c
Sebastian Hilger <hilger@kraenk.de>
2023-05-23 12:01:21 +0200

    remove old theme

--- resources/themes/binario/views/components/content-blocks/text.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-<h2>
-{{ $field->headline }}
-</h2>
-
-<div>
-    text
-</div>
--- resources/themes/default/views/page.blade.php
+++ resources/themes/default/views/page.blade.php
@@ -1,10 +1,10 @@
 <x-layout.layout :browserTitle="($page->browser_title ?: $page->title)">
-
     @if($page->contentBlocks())
         @foreach($page->contentBlocks() as $contentBlock)
             <a name="{{ $contentBlock->field->anchor }}"></a>
             <x-parts.content-block
-                :contentBlock="$contentBlock"
+                :field="$contentBlock->field"
+                :layout="$contentBlock->block"
                 :loop="$loop"
                 context="page"
             />
